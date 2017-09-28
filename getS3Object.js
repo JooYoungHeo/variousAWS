78e4b61605d461c5a3777c171cad23192b13b57b
JooYoungHeo <jyhur900226@naver.com>
2017-09-28 20:47:41 +0900

    [작업] get all s3 objects (over 1000) - use async.whilst

--- getS3Object.js
+++ getS3Object.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+let async = require('async');
 
 let s3 = new AWS.S3();
 let bucketName = 'shopping-guide-bucket';
@@ -6,8 +7,31 @@ let params = {
 	Bucket: bucketName,
 	Prefix: 'gallery/thumbs/'
 };
+let flag = true;
+let files = [];
 
-s3.listObjects(params, (err, data) => {
-	if (err) console.error(err);
-	else console.log(data);
-});
+async.whilst(
+	function() { return flag; },
+	function(cb) {
+		s3.listObjects(params, (err, data) => {
+			if (err) return;
+
+			files = files.concat(data.Contents);
+			
+			if (data.IsTruncated) {
+				let length = data.Contents.length;
+				let marker = data.Contents[length - 1].Key;
+				params.Marker = marker;
+				setTimeout(cb, 100);
+			} else {
+				files.forEach(item => {
+					console.log(item.Key);
+				});
+				flag = false;
+			}
+		});
+	},
+	function(err) {
+		console.log(files);
+	}
+);
